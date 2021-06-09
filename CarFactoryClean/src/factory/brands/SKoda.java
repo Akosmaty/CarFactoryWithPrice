60bec71521d72c21f254664fec1c28f6dee1c3dd
Akosmaty <83218806+Akosmaty@users.noreply.github.com>
2021-06-09 14:58:49 +0200

    Add files via upload

---
+++ CarFactoryClean/src/factory/brands/SKoda.java
@@ -0,0 +1,13 @@
+package factory.brands;
+
+import factory.Brand;
+
+public class SKoda implements Brand {
+
+
+    public String getBrand() {
+        return "Skoda + doplacimy stowe wez pan to od nas.";
+    }
+    public int getBrandPrice(){return 0;}
+
+}
